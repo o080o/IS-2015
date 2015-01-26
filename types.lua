2cc8161cc3e3354814215ec6ec471893a7ef246e
o080o <o0o.moro.o0o@gmail.com>
2015-01-25 18:46:58 -0600

    moved LookupTree into a seperate file

---
+++ types.lua
@@ -0,0 +1,42 @@
+local o = require("object")
+
+local LookupTree
+function getEmptyTree(self, key, private)
+	-- use "value" a special value that will *not* be affected by this
+	-- function
+	if key == "value" then
+		return nil
+	else
+	-- put a new tree into this blank index
+	local tree = LookupTree()
+	self[key] = tree
+	return tree
+	end
+end
+LookupTree = o.class(nil, getEmptyTree)
+-- empty constructor avoids infinite loops when the getter makes a new tree,
+-- attempting to find "__init". if there is no "__init" in the class then the
+-- getter will attempt to find "__init" in the object, causing a recusive
+-- loop.
+function LookupTree:__init()
+end
+function LookupTree:get(list, n)
+	n = n or 1
+	if n > #list then
+		return self.value
+	else
+		return self[ list[n] ]:get(list, n+1)
+	end
+end
+function LookupTree:set(list, value, n)
+	n = n or 1
+	if n > #list then
+		self.value = value
+	else
+		self[list[n]]:set(list, value, n+1 )
+	end
+end
+
+local types = {}
+types.LookupTree = LookupTree
+return types
